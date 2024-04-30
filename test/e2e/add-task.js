bbeb4ea800c0b6ee4cb152154cb5a006c0adaab9
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2024-04-30 21:32:02 +0200

    Update add-task.js

--- test/e2e/add-task.js
+++ test/e2e/add-task.js
@@ -4,13 +4,13 @@ fixture `Add Task`
     .page `http://167.86.105.61:5001/todolist`;
 
 test('Add Task', async t => {
-    t.wait(5000)
+    await t.wait(5000)
     // Find the add-task-text input and set the value to "Test Task"
-    await t.typeText(await Selector(".add-task-text"), "Test Task");
+    await t.typeText(".add-task-text", "Test Task");
     
     // Click the add-task-button button
-    await t.click(await Selector(".add-task-button"));
+    await t.click(".add-task-button");
     
     // Assert that the task was added to the list by checking through task class values
-    await t.expect(await Selector(".task").withText("Test Task").exists).ok();
+    await t.expect(Selector(".task").withText("Test Task").exists).ok();
 });
