b8d5af8e65ccf2cbbb4c36b513e63e97d631c0f1
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2024-04-30 21:47:12 +0200

    Testing

--- test/e2e/add-task.js
+++ test/e2e/add-task.js
@@ -4,7 +4,6 @@ fixture `Add Task`
     .page `http://167.86.105.61:5001/todolist`;
 
 test('Add Task', async t => {
-    await t.wait(5000)
     // Find the add-task-text input and set the value to "Test Task"
     await t.typeText(".add-task-text", "Test Task");
     
