d88d9b415eec59712a850c85c24b0918d1804d15
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2024-05-01 06:15:02 +0200

    e2e test hopefully fixed once and for all
    also made the tasks appear in order of date added

--- test/e2e/todolist-functions.js
+++ test/e2e/todolist-functions.js
@@ -1,31 +1,35 @@
 import { Selector } from 'testcafe';
 
+let randomInt = getRandomInt();
+
 fixture `To-Do List`
     .page `http://167.86.105.61:5001/todolist`;
 
-test('Add Task', async t => {
-    // Find the add-task-text input and set the value to "Test Task"
-    await t.typeText(Selector(".add-task-text"), "Test Task");
-    
-    // Click the add-task-button button
+test("Add Task", async t => {
+    await t.wait(1000)
+    await t.typeText(Selector(".add-task-text"), "Test Task " + randomInt);
     await t.click(Selector(".add-task-button"));
-    
-    // Assert that the task was added to the list by checking through task class values
-    await t.expect(Selector(".task").withText("Test Task").exists).ok();
+
+    await t.wait(1000)
+    await t.expect(Selector(".task").find(".task-text").contains("Test Task" + randomInt)).ok();
 });
 
-test('Mark Task', async t => {
-    // Find task-checkbox and click it
-    await t.click(Selector(".task").withText("Test Task").find(".task-checkbox"));
+test("Mark Task", async t => {
+    await t.wait(1000)
+    await t.click(Selector(".task").find(".task-checkbox"));
 
-    // Assert that the task was marked by checking through task class values and checking for the checkbox being checked
-    await t.expect(Selector(".task").withText("Test Task").find(".task-checkbox").checked).ok();
+    await t.wait(1000)
+    await t.expect(Selector(".task").find(".task-checkbox").checked).ok();
 });
 
-test('Delete Task', async t => {
-    // Click the delete-task-button button
-    await t.click(Selector(".task").withText("Test Task").find(".task-button"));
+test("Delete Task", async t => {
+    await t.wait(1000)
+    await t.click(Selector(".task").find(".task-button"));
 
-    // Assert that the task was deleted from the list by checking through task class values
-    await t.expect(Selector(".task").withText("Test Task").exists).notOk();
+    await t.wait(1000)
+    await t.expect(Selector(".task").find(".task-text").notContains("Test Task" + randomInt)).ok();
 });
+
+function getRandomInt() {
+    return Math.floor(Math.random() * 100) + 1;
+}
