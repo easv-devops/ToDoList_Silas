1b3dc124b80548c3631099fcf1475b2a677c0344
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2024-05-01 06:33:14 +0200

    Fixed e2e test once and for all and fixed list not sorting when adding new task

--- test/e2e/todolist-functions.js
+++ test/e2e/todolist-functions.js
@@ -1,17 +1,18 @@
 import { Selector } from 'testcafe';
 
 let randomInt = getRandomInt();
+let textContent = "Test Task " + randomInt;
 
 fixture `To-Do List`
     .page `http://167.86.105.61:5001/todolist`;
 
 test("Add Task", async t => {
     await t.wait(1000)
-    await t.typeText(Selector(".add-task-text"), "Test Task " + randomInt);
+    await t.typeText(Selector(".add-task-text"), textContent);
     await t.click(Selector(".add-task-button"));
 
     await t.wait(1000)
-    await t.expect(Selector(".task").find(".task-text").contains("Test Task" + randomInt)).ok();
+    await t.expect(Selector(".task").find(".task-text").value).contains(textContent);
 });
 
 test("Mark Task", async t => {
@@ -27,7 +28,7 @@ test("Delete Task", async t => {
     await t.click(Selector(".task").find(".task-button"));
 
     await t.wait(1000)
-    await t.expect(Selector(".task").find(".task-text").notContains("Test Task" + randomInt)).ok();
+    await t.expect(Selector(".task").find(".task-text").value).notContains(textContent);
 });
 
 function getRandomInt() {
