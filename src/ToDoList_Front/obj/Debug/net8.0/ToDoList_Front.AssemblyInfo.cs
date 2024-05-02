ef50fda194f2c617929875191d4ba654b28bdd54
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2024-05-02 21:20:14 +0200

    Style changes, renamed to "Tasks" and press enter to add task

--- src/ToDoList_Front/obj/Debug/net8.0/ToDoList_Front.AssemblyInfo.cs
+++ src/ToDoList_Front/obj/Debug/net8.0/ToDoList_Front.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList_Front")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7870a1e7379bed22292291633cf558e4e16b80de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c69e78cda921d53a8422d1ccb934fc2fe771abb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList_Front")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList_Front")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
