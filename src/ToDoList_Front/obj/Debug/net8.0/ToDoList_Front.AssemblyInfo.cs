33497253f6383862d6998c6456a10324b2b5702b
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2024-05-02 06:23:50 +0200

    CSS clean-up and button for checkboxes

--- src/ToDoList_Front/obj/Debug/net8.0/ToDoList_Front.AssemblyInfo.cs
+++ src/ToDoList_Front/obj/Debug/net8.0/ToDoList_Front.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList_Front")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a511b64cbdc481ffe79a37b94a49df224f686335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7870a1e7379bed22292291633cf558e4e16b80de")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList_Front")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList_Front")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
