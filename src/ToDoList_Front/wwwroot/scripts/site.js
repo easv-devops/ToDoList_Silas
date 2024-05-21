0fedb19fedf34cb2e169d1d93ce480d1e80ee47e
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2024-05-21 05:09:13 +0200

    Made dark mode a little darker

--- src/ToDoList_Front/wwwroot/scripts/site.js
+++ src/ToDoList_Front/wwwroot/scripts/site.js
@@ -1,6 +1,6 @@
 ﻿function switchTheme(isDark) {
     if (isDark) {
-        document.documentElement.style.setProperty('--background-color', '#313131');
+        document.documentElement.style.setProperty('--background-color', '#2a2a2a');
     } else {
         document.documentElement.style.setProperty('--background-color', '#f6f6f6');
     }
