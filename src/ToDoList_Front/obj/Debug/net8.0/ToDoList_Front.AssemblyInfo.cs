31b05be5072ff6ea43aa7c954cc4699e282b6504
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2024-05-21 05:38:29 +0200

    Slight dark mode adjustments

--- src/ToDoList_Front/obj/Debug/net8.0/ToDoList_Front.AssemblyInfo.cs
+++ src/ToDoList_Front/obj/Debug/net8.0/ToDoList_Front.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList_Front")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f44b2b4d8c860f8b24a4d9d71163a596ad0a36e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee71b2a2d829faa7eb59e6cfcd80a50b58f704d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList_Front")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList_Front")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
